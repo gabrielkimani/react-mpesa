254b71bb512a79e021c25c14f670eab33070012f
gabby429 <65331891+gabrielkimani@users.noreply.github.com>
2022-05-02 04:00:39 +0300

    Updates

---
+++ precache-manifest.0651503a5b005ffc9fb9429d307c1286.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b85c5200b2ce8e82e5c7c0be63c4811",
+    "url": "./index.html"
+  },
+  {
+    "revision": "634a5e41b8e00aefb9d1",
+    "url": "./static/css/main.cff15b6f.chunk.css"
+  },
+  {
+    "revision": "cf5eeb2751ce084c41d4",
+    "url": "./static/js/2.b9cd4ae1.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.b9cd4ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "634a5e41b8e00aefb9d1",
+    "url": "./static/js/main.9ddbb4bd.chunk.js"
+  },
+  {
+    "revision": "b44b0babb974c95c492b",
+    "url": "./static/js/runtime-main.c055b53d.js"
+  }
+]);
